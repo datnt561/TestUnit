9a920c5a6cf641d736610c7edf31a954b241bfd4
datnt561 <datnt93_it@yahoo.com>
2015-09-16 13:27:13 +0700

    up date

--- src/QuadraticEquationSolver.java
+++ src/QuadraticEquationSolver.java
@@ -1,9 +1,102 @@
-
+import java.util.Scanner;
+import java.lang.Math;
 public class QuadraticEquationSolver {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	double a, b, c;
+
+	public QuadraticEquationSolver() {
+	}
+
+	public QuadraticEquationSolver(double a, double b, double c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
 
 	}
+
+	public void input() {
+		Scanner input = new Scanner(System.in);
+		setA(input);
+		setB(input);
+		setC(input);
+		input.close();
+	}
+
+	public void setA(Scanner input) {
+
+		System.out.print("Nhap a = ");
+		a = input.nextFloat();
+
+	}
+
+	public double getA() {
+		return a;
+	}
+
+	public void setB(Scanner input) {
+
+		System.out.print("Nhap b = ");
+		b = input.nextFloat();
+
+	}
+
+	public double getB() {
+		return b;
+	}
+
+	public void setC(Scanner input) {
+
+		System.out.print("Nhap c = ");
+		c = input.nextFloat();
+
+	}
+
+	public double getC() {
+		return c;
+	}
+
+	public double computeDelta() {
+		return b * b - 4 * a * c;
+	}
+
+	public double returnX1() {
+		double delta = computeDelta();
+		return (-b + Math.sqrt(delta)) / (2 * a);
+	}
+
+	public double returnX2() {
+		double delta = computeDelta();
+		return (-b - Math.sqrt(delta)) / (2 * a);
+	}
+
+	public double returnX() {
+		return -b / (2 * a);
+	}
+
+	public static void main(String[] args) {
+		QuadraticEquationSolver pt = new QuadraticEquationSolver();
+		pt.input();
+		if (pt.getA() == 0) {
+			if (pt.getB() == 0) {
+				if (pt.getC() == 0)
+					System.out.println("Phuong trinh vo so nghiem");
+				else
+					System.out.println("Phuong trinh vo nghiem");
+			} else {
+				System.out.println("Phuong trinh co 1 nghiem la:");
+				System.out.println(pt.returnX());
+			}
+
+		} else {
+			if (pt.computeDelta() > 0) {
+				System.out.println("Phuong trinh co 2 nghiem la:");
+				System.out.println(pt.returnX1());
+				System.out.println(pt.returnX2());
+			} else
+				System.out.println("Phuong trinh vo nghiem");
+
+		}
+	}
+
 
 }
--- src/TestQuadratic.java
+++ src/TestQuadratic.java
@@ -1,12 +1,43 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestQuadratic {
 
+	private QuadraticEquationSolver p;
+
+	@Before
+	public void setUp(){
+		p = new QuadraticEquationSolver(2, 3, -5);
+	}
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testTestInput() {
+		
+		assertEquals(2, p.getA());
+		assertEquals(3, p.getB());
+		assertEquals(-5, p.getC());
+	}
+
+	@Test
+	public void testTestDelta() {
+		assertEquals(-32, p.computeDelta());
+	}
+
+	@Test
+	public void testTestX() {
+		if(p.computeDelta() == 0){
+			assertEquals(3, p.returnX());
+		}
+	}
+
+	@Test
+	public void testTestX1AndX2() {
+		if(p.computeDelta() > 0){
+			assertEquals(1, p.returnX1());
+			assertEquals(-5.0/2, p.returnX2());
+		}
 	}
 
 }
